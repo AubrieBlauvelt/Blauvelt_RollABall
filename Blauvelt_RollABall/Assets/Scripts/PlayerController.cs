a4788237164a76a5272170efdc8adb4cb04822b2
Aubrie Blauvelt <aublauve@iu.edu>
2020-10-06 20:55:27 -0400

    Decrease Pickup
    added a new prefab for a decrease pickup item and added the coding for it as well.

--- Blauvelt_RollABall/Assets/Scripts/PlayerController.cs
+++ Blauvelt_RollABall/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,10 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
     public TextMeshProUGUI countText;
-    public GameObject winTextObject; 
+    public GameObject winTextObject;
 
     private Rigidbody rb;
-    private int count; 
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >=12)
+        if (count >= 12)
         {
             winTextObject.SetActive(true);
         }
@@ -47,12 +47,12 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement * speed); 
+        rb.AddForce(movement * speed);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
@@ -63,9 +63,23 @@ public class PlayerController : MonoBehaviour
             float growthAmount = .4f;
             transform.localScale += new Vector3(growthAmount, growthAmount, growthAmount);
         }
+        
+        //Made a new tag and prefab. "decrease pickup" is what I named both the tag and prefab. 
+        if (other.gameObject.CompareTag("Decrease Pickup"))
+        {
+            //Allows me to decrease the score upon collision with the decrease pickups
+            other.gameObject.SetActive(false);
+            count = count - 1;
+
+            SetCountText();
+
+            // On collosion with the decrease pickup, the player's size will decrease.  
+            float growthAmount = -.4f;
+            transform.localScale += new Vector3(growthAmount, growthAmount, growthAmount);
+        }
     }
 
-        //To enact a reset on falling off the map
+    //To enact a reset on falling off the map
     private void Update()
     {
         if (transform.position.y < -10f)  //sets the fall distance before reset
